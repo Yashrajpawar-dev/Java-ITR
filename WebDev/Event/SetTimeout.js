55c78a9d8c6febb69e7d4f83414e7de37b31fb9c
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-07-14 14:24:07 +0530

    some changes in file

---
+++ WebDev/Event/SetTimeout.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+function dataOne(){
+    console.log("data 1");
+    
+}
+//setTimeout(dataOne,dataTwo,dataThree,3000)
+
+
+function dataTwo(){
+    console.log("data 2");
+    
+}
+//setTimeout(dataTwo,6000)
+
+
+function dataThree(){
+    console.log("data 3");
+    
+}
+//setTimeout(dataThree,9000)
+
+let arrowExe = ()=>{
+ function dataOne(){
+    console.log("data 1");
+} 
+dataOne();
+function datafive(){
+   for (let index = 0; index < 11; index++) {
+   console.log("hello");
+   
+    
+   }
+} 
+datafive();
+
+}
+setTimeout(arrowExe,10000)
